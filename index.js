37d7d43a50affa9e59dbd139d45f16338a8f3c7e
Jacky <jacky@JackydeMacBook-Pro.local>
2020-02-13 17:28:44 +0800

    basic test finish

--- index.js
+++ index.js
@@ -6,46 +6,49 @@ const Kendo2JsonLogic = function (rule) {
 function analyzeOperator(rule) {
 
   if (rule.operator.includes('not')) {
-    rule.operator = negativeDictionary[rule.operator]
+    var logic = {
+      ...rule
+    }
+    logic.operator = negativeDictionary[logic.operator]
     return {
-      'and': [analyzeOperator(rule), false]
+      '!': [analyzeOperator(logic)]
     }
   }
 
   else {
     switch (rule.operator) {
       case 'eq':
-        return { '==': [rule.field, rule.value] }
+        return { '==': [{'var': rule.field}, rule.value] }
 
       case 'neq':
-        return { '!=': [rule.field, rule.value] }
+        return { '!=': [{'var': rule.field}, rule.value] }
 
       case 'lt':
-        return { '<': [rule.field, rule.value] }
+        return { '<': [{'var': rule.field}, rule.value] }
 
       case 'lte':
-        return { '<=': [rule.field, rule.value] }
+        return { '<=': [{'var': rule.field}, rule.value] }
 
       case 'gt':
-        return { '>': [rule.field, rule.value] }
+        return { '>': [{'var': rule.field}, rule.value] }
 
       case 'gte':
-        return { '>=': [rule.field, rule.value] }
+        return { '>=': [{'var': rule.field}, rule.value] }
 
       case 'contains':
-        return { 'in': [rule.value, rule.field] }
+        return { 'in': [rule.value, {'var': rule.field}] }
 
       case 'isnull':
-        return { '==': [rule.field, null] }
+        return { '==': [{'var': rule.field}, null] }
 
       case 'startswith':
-        return { '==': [rule.value, { 'substr': [{ 'var': rule.field }, 0, rule.value.length - 1] }] }
+        return { '==': [rule.value, { 'substr': [{'var': rule.field}, 0, rule.value.length] }] }
 
       case 'endswith':
-        return { '==': [rule.value, { 'substr': [{ 'var': rule.field }, 1 - rule.value.length] }] }
+        return { '==': [rule.value, { 'substr': [{'var': rule.field}, -rule.value.length] }] }
 
       case 'isempty':
-        return { '==': [rule.field, ''] }
+        return { '==': [{'var': rule.field}, ''] }
     }
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,18 +1,35 @@
 const Kendo2JsonLogic = require('../index.js')
 const fs = require('fs')
+const jsonLogic = require('json-logic-js')
 
 var testRule = [
-  { field: "name", operator: "startswith", value: "Jane" },
-  { field: "category", operator: "eq", value: "Beverages" },
-  { field: "name", operator: "neq", value: "Coffee" },
-  { field: "category", operator: "eq", value: "Food" },
   { field: "name", operator: "eq", value: "Tea" },
-  { field: "name", operator: "startswith", value: "Jane" }
+  { field: "name", operator: "eq", value: "Food" },
+  { field: "name", operator: "neq", value: "Tea" },
+  { field: "name", operator: "neq", value: "Food" },
+  { field: "name", operator: "isnull"},
+  { field: "name", operator: "isnotnull"},
+  { field: "price", operator: "lt", value: 10},
+  { field: "price", operator: "lte", value: 10},
+  { field: "price", operator: "gt", value: 10},
+  { field: "price", operator: "gte", value: 10},
+  { field: "category", operator: "startswith", value: "B" },
+  { field: "category", operator: "doesnotstartwith", value: "B" },
+  { field: "category", operator: "endswith", value: "s" },
+  { field: "category", operator: "doesnotendwith", value: "s" },
+  { field: "category", operator: "contains", value: "ver" },
+  { field: "category", operator: "doesnotcontain", value: "ver" },
+  { field: "category", operator: "isempty"},
+  { field: "category", operator: "isnotempty"}
 ]
+
+var data = { name: "Tea", category: "Beverages", price: 10 }
 
 var result = []
 testRule.forEach(e => {
-  result.push(Kendo2JsonLogic(e));
+  let tmp = Kendo2JsonLogic(e)
+  result.push(tmp);
+  console.log(e, jsonLogic.apply(tmp, data))
 })
 
-fs.writeFile('./result.json', JSON.stringify(result), e => console.log('finish'))
+fs.writeFile('./test/result.json', JSON.stringify(result), e => console.log('finish'))
