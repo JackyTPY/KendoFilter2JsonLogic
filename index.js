c20f5ccb90c9af22b58f8589e31bed50bae6a0e6
Jacky <jacky@JackydeMacBook-Pro.local>
2020-02-14 12:07:33 +0800

    finish the test

--- index.js
+++ index.js
@@ -1,64 +1,81 @@
+
+
 const Kendo2JsonLogic = function (rule) {
-  let result = analyzeOperator(rule)
+  let result = analyzeLogic(rule)
   return result
 }
 
-function analyzeOperator(rule) {
+function analyzeLogic(rule) {
+  let result = []
 
-  if (rule.operator.includes('not')) {
-    var logic = {
-      ...rule
-    }
-    logic.operator = negativeDictionary[logic.operator]
-    return {
-      '!': [analyzeOperator(logic)]
-    }
-  }
+  switch (rule.logic) {
+    case 'and':
+      rule.filters.forEach(e => {
+        result.push(analyzeLogic(e))
+      })
+      return { 'and': result }
 
-  else {
-    switch (rule.operator) {
-      case 'eq':
-        return { '==': [{'var': rule.field}, rule.value] }
+    case 'or':
+      rule.filters.forEach(e => {
+        result.push(analyzeLogic(e))
+      })
+      return { 'or': result }
 
-      case 'neq':
-        return { '!=': [{'var': rule.field}, rule.value] }
-
-      case 'lt':
-        return { '<': [{'var': rule.field}, rule.value] }
-
-      case 'lte':
-        return { '<=': [{'var': rule.field}, rule.value] }
-
-      case 'gt':
-        return { '>': [{'var': rule.field}, rule.value] }
-
-      case 'gte':
-        return { '>=': [{'var': rule.field}, rule.value] }
-
-      case 'contains':
-        return { 'in': [rule.value, {'var': rule.field}] }
-
-      case 'isnull':
-        return { '==': [{'var': rule.field}, null] }
-
-      case 'startswith':
-        return { '==': [rule.value, { 'substr': [{'var': rule.field}, 0, rule.value.length] }] }
-
-      case 'endswith':
-        return { '==': [rule.value, { 'substr': [{'var': rule.field}, -rule.value.length] }] }
-
-      case 'isempty':
-        return { '==': [{'var': rule.field}, ''] }
-    }
+    default:
+      return analyzeOperator(rule)
   }
 }
 
-var negativeDictionary = {
-  "isnotnull": "isnull",
-  "doesnotstartwith": "startswith",
-  "doesnotendwith": "endswith",
-  "doesnotcontain": "contains",
-  "isnotempty": "isempty",
+function analyzeOperator(rule) {
+  switch (rule.operator) {
+    case 'eq':
+      return { '==': [{ 'var': rule.field }, rule.value] }
+
+    case 'neq':
+      return { '!=': [{ 'var': rule.field }, rule.value] }
+
+    case 'lt':
+      return { '<': [{ 'var': rule.field }, rule.value] }
+
+    case 'lte':
+      return { '<=': [{ 'var': rule.field }, rule.value] }
+
+    case 'gt':
+      return { '>': [{ 'var': rule.field }, rule.value] }
+
+    case 'gte':
+      return { '>=': [{ 'var': rule.field }, rule.value] }
+
+    case 'contains':
+      return { 'in': [rule.value, { 'var': rule.field }] }
+
+    case 'doesnotcontain':
+      return { '!': [{ 'in': [rule.value, { 'var': rule.field }] }] }
+
+    case 'isnull':
+      return { '==': [{ 'var': rule.field }, null] }
+
+    case 'isnotnull':
+      return { '!=': [{ 'var': rule.field }, null] }
+
+    case 'startswith':
+      return { '==': [rule.value, { 'substr': [{ 'var': rule.field }, 0, rule.value.length] }] }
+
+    case 'doesnotstartwith':
+      return { '!=': [rule.value, { 'substr': [{ 'var': rule.field }, 0, rule.value.length] }] }
+
+    case 'endswith':
+      return { '==': [rule.value, { 'substr': [{ 'var': rule.field }, -rule.value.length] }] }
+
+    case 'doesnotendwith':
+      return { '!=': [rule.value, { 'substr': [{ 'var': rule.field }, -rule.value.length] }] }
+
+    case 'isempty':
+      return { '!': [{ 'var': rule.field }] }
+
+    case 'isnotempty':
+      return { '!!': [{ 'var': rule.field }] }
+  }
 }
 
 module.exports = Kendo2JsonLogic
--- test/test.js
+++ test/test.js
@@ -2,34 +2,25 @@ const Kendo2JsonLogic = require('../index.js')
 const fs = require('fs')
 const jsonLogic = require('json-logic-js')
 
-var testRule = [
-  { field: "name", operator: "eq", value: "Tea" },
-  { field: "name", operator: "eq", value: "Food" },
-  { field: "name", operator: "neq", value: "Tea" },
-  { field: "name", operator: "neq", value: "Food" },
-  { field: "name", operator: "isnull"},
-  { field: "name", operator: "isnotnull"},
-  { field: "price", operator: "lt", value: 10},
-  { field: "price", operator: "lte", value: 10},
-  { field: "price", operator: "gt", value: 10},
-  { field: "price", operator: "gte", value: 10},
-  { field: "category", operator: "startswith", value: "B" },
-  { field: "category", operator: "doesnotstartwith", value: "B" },
-  { field: "category", operator: "endswith", value: "s" },
-  { field: "category", operator: "doesnotendwith", value: "s" },
-  { field: "category", operator: "contains", value: "ver" },
-  { field: "category", operator: "doesnotcontain", value: "ver" },
-  { field: "category", operator: "isempty"},
-  { field: "category", operator: "isnotempty"}
-]
+var rules = fs.readFileSync('./test/nest.json', 'utf-8');
+rules = JSON.parse(rules)
 
-var data = { name: "Tea", category: "Beverages", price: 10 }
+var data = fs.readFileSync('./test/menu.json', 'utf-8');
+data = JSON.parse(data)
 
-var result = []
-testRule.forEach(e => {
-  let tmp = Kendo2JsonLogic(e)
-  result.push(tmp);
-  console.log(e, jsonLogic.apply(tmp, data))
-})
+var result = Kendo2JsonLogic(rules)
+
+var temp = {
+  "filter": [
+    { "var": "" },
+    result
+  ]
+}
+
+var tempr = jsonLogic.apply(temp, data)
+
+
+console.log(tempr)
+console.log('---- ' + tempr.length + ' items ----')
 
 fs.writeFile('./test/result.json', JSON.stringify(result), e => console.log('finish'))
